33c9e36840e330b4a7a658705407fe520a97c310
Girish Mahajan <meetgirish.mjn@gmail.com>
2020-04-10 23:25:00 +0530

    sendgrid mail gateway

--- ShunQApiSol/BusinessCore/Services/GmailGateway.cs
+++ ShunQApiSol/BusinessCore/Services/GmailGateway.cs
@@ -6,12 +6,39 @@ using System.Text;
 using System.Threading.Tasks;
 using BusinessCore.Services.Contracts;
 using BusinessCore.Services.Models;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace BusinessCore.Services
 {
     public class GmailGateway : IEmailGateway
     {
         public async Task<MailSendResult> SendMail(string to, string subject, string body)
+        {
+            var result = new MailSendResult();
+            try
+            {
+                const string API_KEY = "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";
+
+                var client = new SendGridClient(API_KEY);
+
+                SendGridMessage mailMessage = new SendGridMessage();
+                mailMessage.SetFrom("Shunq.CareServices.v1@gmail.com");
+                mailMessage.AddTo(to);
+                mailMessage.AddContent(MimeType.Html, body);
+                mailMessage.SetSubject(subject);
+                var response = await client.SendEmailAsync(mailMessage);
+            }
+            catch (Exception ex)
+            {
+                result.Status = ex.Message;
+                result.HasError = true;
+            }
+            return result;
+        }
+
+        [Obsolete]
+        public async Task<MailSendResult> SendMail_Obsolete(string to, string subject, string body)
         {
             var result = new MailSendResult();
             try
